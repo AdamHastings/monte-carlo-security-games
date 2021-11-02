473a24d6eff8b1c0651d5db1e82a0f4706171e49
adamhastings <itsadamhastings@gmail.com>
2021-11-02 18:51:22 -0400

    parameterize some variables

--- configs/cfg1.py
+++ configs/cfg1.py
@@ -9,17 +9,17 @@ import numpy as np
 # 3. PAYOFF (percentage of wealth transferred after a sucessful attack)
 
 params = dict(
-    PERCENT_EVIL = 0.37,
+    PERCENT_EVIL = 0.5, # almost seems irrelevant: simply determines how fast the system converges
     PAYOFF = 0.1,
-    WEALTH_GAP = 0.2,
+    WEALTH_GAP = 0.1,
     SEC_INVESTMENT_CONVERSION_RATE = 0.1,
     ATTACK_COST_CONVERSION_RATE = 0.01,
     SEC_INVESTMENT = 0.1
 )
 
 game_settings = dict(
-    BLUE_PLAYERS = 1000,
-    SIM_ITERS = 500,
+    BLUE_PLAYERS = 2000,
+    SIM_ITERS = 1000,
     NUM_GAMES = 1,
 )
 
