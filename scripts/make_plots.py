b8673f4cefb1ecf2f28c31117fff7f7a32b87e61
adamhastings <itsadamhastings@gmail.com>
2021-11-14 13:50:35 -0500

    no more hist! better style

--- scripts/make_plots.py
+++ scripts/make_plots.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 from termcolor import colored
 
 
@@ -10,6 +11,7 @@ def crossover_hist(df):
     
     # fig,a =  plt.subplots(2,5,sharey=True)
 
+    custom_lines = []
     mandates = sorted(df['SEC_INVESTMENT'].unique())
     for i,m in enumerate(mandates):
         if (m == 1):
@@ -19,15 +21,17 @@ def crossover_hist(df):
         mands = df.loc[df['SEC_INVESTMENT'] == m]
         crossovers = mands['crossover'].to_numpy()
         crossovers = np.where(crossovers == -1, -100, crossovers)
-        print(crossovers.shape)
-        row = i//5
-        col = i%5
+        # print(crossovers.shape)
+        # row = i//5
+        # col = i%5
         plt.hist(crossovers, bins=100, cumulative=True,  histtype='step', label=str(m * 100) + "% mandate", density=True)
+        custom_lines.append(Line2D([0], [0], lw=4))
         # patches[0].set_fc('r')
         # a[row, col].set_title("Mandate = " + str(m * 100) + "%")
-    plt.yscale("log")
-    plt.title("CDF of crossover iterations (log scale)")
-    plt.legend()
+    # plt.yscale("log")
+    plt.title("CDF of crossover iterations (linear scale)")
+    print(mandates)
+    plt.legend(custom_lines, mandates)
     plt.xlabel("Iteration")
     plt.ylabel("Count")
     # plt.xlabel("Crossover iteration (far-left red bar means no crossover)")
@@ -49,8 +53,8 @@ def converge_hist(df):
         plt.hist(convergences, bins=100,  cumulative=True,  histtype='step', label=str(m * 100) + "% mandate", density=True)
 
     # plt.ylim(0,10000)
-    plt.title("CDF of convergence iterations (log scale)")
-    plt.yscale("log")
+    plt.title("CDF of convergence iterations (linear scale)")
+    # plt.yscale("log")
     plt.xlabel("Iteration")
     plt.ylabel("Count")
     plt.legend()
@@ -62,6 +66,8 @@ def loss_ratio_hist(df):
     # fig, a =  plt.subplots(2,5,sharey=True,sharex=True)
 
     mandates = sorted(df['SEC_INVESTMENT'].unique())
+
+    custom_lines = []
     for i,m in enumerate(mandates):
         if (m == 1):
             continue
@@ -79,25 +85,51 @@ def loss_ratio_hist(df):
 
         ratios = []
         for start,finish in zip(d_inits, d_ends):
-            ratios.append((start - finish)/start)
+            ratios.append(((start - finish)/start) * 100)
         
         # row = i//5
         # col = i%5
         # print(row, col)
         # a[0, 0].hist(np.zeros(100), bins=100)
-        plt.hist(ratios, bins=np.linspace(0,1,100),  cumulative=True,  histtype='step', label=str(m * 100) + "% mandate", density=True)
+        # plt.hist(ratios, bins=np.linspace(0,100,100),  cumulative=True,  histtype='step', label=str(m * 100) + "% mandate", density=True)
+        # custom_lines.append(Line2D([0], [0], color=cmap(m), lw=4))
         # a[row, col].set_title("Mandate = " + str(m * 100) + "%")
         # a[row, col].set_ylim(bottom=1)
         # a[row, col].yscale("log")
+
+        N = 100000
+        X2 = np.sort(ratios)
+        F2 = np.array(range(N))/float(N) * 100
+
+        plt.step(X2, F2, label=str(m * 100) + "% mandate")
+
+        # sorted_ratios = sorted(ratios)
+        # print(sorted_ratios)
+        # cdf = []
+        # for i,r in enumerate(sorted_ratios):
+        #     if i == 0:
+        #         cdf.append(r)
+        #     else:
+        #         cdf.append(cdf[i-1] + r)
+
+        # x = np.arange(len(cdf))
+        # # print(x)
+        # # print(cdf)
+        # plt.step(x, cdf)
+        # plt.show()
+            
     # plt.ylim(0, 1*10**6)
 
     # plt.yscale("log")
     # plt.ylim(0,10000)
-    plt.title("CDF of percent of assets lost (linear scale)")
-    plt.xlabel("% of assets lost")
-    plt.ylabel("Count")
+    plt.title("CDF of percent of assets lost")
+    plt.xlabel("Percent of assets lost")
+    plt.ylabel("Percent of games")
+    # plt.xlim(0,100)
+    print(mandates)
+    plt.legend(loc="lower right")
     # plt.tight_layout()
-    plt.legend()
+    # plt.legend(loc="lower right")
     plt.show()  
 
 def make_plots(df):
