07afd0dffaf7b2ffa7ce9fd8f598c82c679f0e12
adamhastings <itsadamhastings@gmail.com>
2021-11-14 16:29:28 -0500

    some tick stuff and clipping

--- scripts/make_plots.py
+++ scripts/make_plots.py
@@ -5,6 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 from termcolor import colored
+from matplotlib.ticker import AutoMinorLocator, FormatStrFormatter
+
 
 
 def crossover_hist(df):
@@ -50,26 +52,28 @@ def converge_hist(df):
 
     mandates = sorted(df['SEC_INVESTMENT'].unique())
     for i,m in enumerate(mandates):
-        if (m == 1):
+        if (m == 1 or m == 0.9):
             continue
 
         mands = df.loc[df['SEC_INVESTMENT'] == m]
         convergences = mands['final_iter'].to_numpy()
         convergences = convergences - 50
 
-        # plt.hist(convergences, bins=100,  cumulative=True,  histtype='step', label=str(m * 100) + "% mandate", density=True)
         N = 100000
         X2 = np.sort(convergences)
         F2 = np.array(range(N))/float(N) * 100
 
-        plt.step(X2, F2, label=str(m * 100) + "% mandate")
+        plt.step(X2, F2, label=str(int(m * 100)) + "%")
 
-    # plt.ylim(0,10000)
+    plt.ylim(60,100)
     plt.title("CDF of convergence iterations")
     # plt.yscale("log")
+    plt.xlim(left=0, right=1250)
+    plt.minorticks_on()
+    plt.grid(True, which='both')
     plt.xlabel("Iteration")
     plt.ylabel("Percent of games")
-    plt.legend()
+    plt.legend(loc="lower right", title="Mandate:")
     # plt.tight_layout()
     plt.show()  
 
@@ -101,24 +105,8 @@ def loss_ratio_hist(df):
         for start,finish in zip(d_inits, d_ends):
             ratios.append(((start - finish)/start) * 100)
         
-        # row = i//5
-        # col = i%5
-        # print(row, col)
-        # a[0, 0].hist(np.zeros(100), bins=100)
-        # plt.hist(ratios, bins=np.linspace(0,100,100),  cumulative=True,  histtype='step', label=str(m * 100) + "% mandate", density=True)
-        # custom_lines.append(Line2D([0], [0], color=cmap(m), lw=4))
-        # a[row, col].set_title("Mandate = " + str(m * 100) + "%")
-        # a[row, col].set_ylim(bottom=1)
-        # a[row, col].yscale("log")
-
         N = 100000
         X2 = np.sort(ratios)
-        # X2 = X2[X2 != m * 100]
-        # N = X2.shape[0]
-        # print(N)
-
-        # X3 = np.delete(X2,np.arange(100))
-        # print(X3.shape)
         F2 = np.array(range(N))/float(N) * 100
         
         # F2 = np.delete(F2,np.arange(100))
@@ -132,42 +120,24 @@ def loss_ratio_hist(df):
         print("count: " + str(count))
         X2 = np.delete(X2, np.arange(count))
         F2 = np.delete(F2, np.arange(count))
-        # print(X2[0])
-        # print(m * 100)
-        # print(X2)
-        # print(F2)
+
         print("---------------")
         # F2 = 100 - F2
         plt.plot(X2, F2, label=str(int(m * 100)) + "%")
 
-        # sorted_ratios = sorted(ratios)
-        # print(sorted_ratios)
-        # cdf = []
-        # for i,r in enumerate(sorted_ratios):
-        #     if i == 0:
-        #         cdf.append(r)
-        #     else:
-        #         cdf.append(cdf[i-1] + r)
 
-        # x = np.arange(len(cdf))
-        # # print(x)
-        # # print(cdf)
-        # plt.step(x, cdf)
-        # plt.show()
-            
-    # plt.ylim(0, 1*10**6)
-
-    # plt.yscale("log")
-    # plt.ylim(0,10000)
-    plt.title("CDF of percent of assets lost")
-    plt.xlabel("Percent of assets lost (mandated spending + losses from attacks)")
-    plt.ylabel("Percent of games")
+        
+    plt.title("CDF of percent decrease in assets")
+    plt.xlabel("Percent decrease in assets (mandated spending + losses from attacks)")
+    plt.ylabel("Percent of simulations")
     plt.xlim(0,100)
-    plt.ylim(0,100)
+    plt.ylim(60,100)
+    # plt.xaxis.set_minor_locator(AutoMinorLocator())
+    # plt.xaxis.set_minor_formatter(FormatStrFormatter("%.3f"))
     print(mandates)
     plt.minorticks_on()
-    plt.legend(loc="lower right", title="Mandate:")
     plt.grid(True, which='both')
+    plt.legend(loc="lower right", title="Mandate:")
     # plt.tight_layout()
     # plt.legend(loc="lower right")
     plt.show()  
