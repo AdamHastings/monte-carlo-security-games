b305815450b03decfcfc69a1dcb9293d46d446be
adamhastings <itsadamhastings@gmail.com>
2021-11-14 15:00:06 -0500

    no more hists

--- scripts/make_plots.py
+++ scripts/make_plots.py
@@ -20,20 +20,27 @@ def crossover_hist(df):
 
         mands = df.loc[df['SEC_INVESTMENT'] == m]
         crossovers = mands['crossover'].to_numpy()
-        crossovers = np.where(crossovers == -1, -100, crossovers)
+        # crossovers = np.where(crossovers == -1, -100, crossovers)
+        no_crossovers = np.count_nonzero(crossovers == -1)
+        print(no_crossovers)
         # print(crossovers.shape)
         # row = i//5
         # col = i%5
-        plt.hist(crossovers, bins=100, cumulative=True,  histtype='step', label=str(m * 100) + "% mandate", density=True)
-        custom_lines.append(Line2D([0], [0], lw=4))
+        # plt.hist(crossovers, bins=100, cumulative=True,  histtype='step', label=str(m * 100) + "% mandate", density=True)
+        # custom_lines.append(Line2D([0], [0], lw=4))
+        N = 100000
+        X2 = np.sort(crossovers)
+        F2 = np.array(range(N))/float(N) * 100 * (N - no_crossovers)/N
+
+        plt.step(X2, F2, label=str(int(m * 100)) + "%")
+        
         # patches[0].set_fc('r')
         # a[row, col].set_title("Mandate = " + str(m * 100) + "%")
     # plt.yscale("log")
-    plt.title("CDF of crossover iterations (linear scale)")
-    print(mandates)
-    plt.legend(custom_lines, mandates)
+    plt.title("CDF of crossover iterations")
+    plt.legend(loc="upper right", title="Mandated security spending")
     plt.xlabel("Iteration")
-    plt.ylabel("Count")
+    plt.ylabel("Percent of games")
     # plt.xlabel("Crossover iteration (far-left red bar means no crossover)")
     # plt.ylabel("Count")
     plt.show()  
@@ -50,15 +57,20 @@ def converge_hist(df):
         convergences = mands['final_iter'].to_numpy()
         convergences = convergences - 50
 
-        plt.hist(convergences, bins=100,  cumulative=True,  histtype='step', label=str(m * 100) + "% mandate", density=True)
+        # plt.hist(convergences, bins=100,  cumulative=True,  histtype='step', label=str(m * 100) + "% mandate", density=True)
+        N = 100000
+        X2 = np.sort(convergences)
+        F2 = np.array(range(N))/float(N) * 100
+
+        plt.step(X2, F2, label=str(m * 100) + "% mandate")
 
     # plt.ylim(0,10000)
-    plt.title("CDF of convergence iterations (linear scale)")
+    plt.title("CDF of convergence iterations")
     # plt.yscale("log")
     plt.xlabel("Iteration")
-    plt.ylabel("Count")
+    plt.ylabel("Percent of games")
     plt.legend()
-    plt.tight_layout()
+    # plt.tight_layout()
     plt.show()  
 
 def loss_ratio_hist(df):
@@ -72,11 +84,13 @@ def loss_ratio_hist(df):
         if (m == 1):
             continue
 
-        print(m)
+        print("*********---------------")
+
+        # print(m)
         mands = df.loc[df['SEC_INVESTMENT'] == m]
 
         d_inits = mands['d_init'].to_numpy() / (1 - float(m))
-        print(d_inits)
+        # print(d_inits)
         d_ends = mands['d_end'].to_numpy()
 
         convergences = mands['final_iter'].to_numpy()
@@ -99,9 +113,32 @@ def loss_ratio_hist(df):
 
         N = 100000
         X2 = np.sort(ratios)
-        F2 = np.array(range(N))/float(N) * 100
+        # X2 = X2[X2 != m * 100]
+        # N = X2.shape[0]
+        # print(N)
 
-        plt.step(X2, F2, label=str(m * 100) + "% mandate")
+        # X3 = np.delete(X2,np.arange(100))
+        # print(X3.shape)
+        F2 = np.array(range(N))/float(N) * 100
+        
+        # F2 = np.delete(F2,np.arange(100))
+        count = 0
+        for i in X2:
+            if int(round(i)) != int(round(m * 100)):
+                break
+            else:
+                count += 1
+
+        print("count: " + str(count))
+        X2 = np.delete(X2, np.arange(count))
+        F2 = np.delete(F2, np.arange(count))
+        # print(X2[0])
+        # print(m * 100)
+        # print(X2)
+        # print(F2)
+        print("---------------")
+        # F2 = 100 - F2
+        plt.plot(X2, F2, label=str(int(m * 100)) + "%")
 
         # sorted_ratios = sorted(ratios)
         # print(sorted_ratios)
@@ -123,11 +160,14 @@ def loss_ratio_hist(df):
     # plt.yscale("log")
     # plt.ylim(0,10000)
     plt.title("CDF of percent of assets lost")
-    plt.xlabel("Percent of assets lost")
+    plt.xlabel("Percent of assets lost (mandated spending + losses from attacks)")
     plt.ylabel("Percent of games")
-    # plt.xlim(0,100)
+    plt.xlim(0,100)
+    plt.ylim(0,100)
     print(mandates)
-    plt.legend(loc="lower right")
+    plt.minorticks_on()
+    plt.legend(loc="lower right", title="Mandate:")
+    plt.grid(True, which='both')
     # plt.tight_layout()
     # plt.legend(loc="lower right")
     plt.show()  
