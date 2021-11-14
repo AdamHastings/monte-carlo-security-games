b6fae7016f6a557cc79a9194000cfa59b262462a
adamhastings <itsadamhastings@gmail.com>
2021-11-14 03:11:40 -0500

    cdfs

--- scripts/make_plots.py
+++ scripts/make_plots.py
@@ -7,46 +7,104 @@ from termcolor import colored
 
 
 def crossover_hist(df):
-    crossovers = df['crossover'].to_numpy()
-    crossovers = np.where(crossovers == -1, -100, crossovers)
-    n, bins, patches = plt.hist(crossovers, bins=100)
-    patches[0].set_fc('r')
+    
+    fig,a =  plt.subplots(2,5,sharey=True)
+
+    mandates = sorted(df['SEC_INVESTMENT'].unique())
+    for i,m in enumerate(mandates):
+        if (m == 1):
+            continue
+
+
+        mands = df.loc[df['SEC_INVESTMENT'] == m]
+        crossovers = mands['crossover'].to_numpy()
+        crossovers = np.where(crossovers == -1, -100, crossovers)
+        print(crossovers.shape)
+        row = i//5
+        col = i%5
+        n, bins, patches = a[row, col].hist(crossovers, bins=100, cumulative=True)
+        patches[0].set_fc('r')
+        a[row, col].set_title("Mandate = " + str(m * 100) + "%")
     plt.yscale("log")
-    plt.title("Histogram of crossover iterations (log scale)")
-    plt.xlabel("Crossover iteration (far-left red bar means no crossover)")
-    plt.ylabel("Count")
-    # plt.show()  
+    plt.suptitle("Histogram of crossover iterations (log scale)")
+    # plt.xlabel("Crossover iteration (far-left red bar means no crossover)")
+    # plt.ylabel("Count")
+    plt.show()  
 
 def converge_hist(df):
-    convergences = df['final_iter'].to_numpy()
-    convergences = convergences - 50
-    plt.hist(convergences, bins=100)
-    plt.yscale("log")
-    plt.title("Histogram of convergence iterations (log scale)")
-    plt.xlabel("Convergence iteration")
-    plt.ylabel("Count")
-    # plt.show()  
+    fig,a =  plt.subplots(2,5,sharey=True, sharex=True)
+
+    mandates = sorted(df['SEC_INVESTMENT'].unique())
+    for i,m in enumerate(mandates):
+        if (m == 1):
+            continue
+
+        mands = df.loc[df['SEC_INVESTMENT'] == m]
+        convergences = mands['final_iter'].to_numpy()
+        convergences = convergences - 50
+        if m == 0.9:
+            print(convergences)
+        row = i//5
+        col = i%5
+        n, bins, patches = a[row, col].hist(convergences, bins=100,  cumulative=True)
+        a[row, col].set_title("Mandate = " + str(m * 100) + "%")
+
+    # plt.ylim(0,10000)
+    plt.suptitle("Histogram of convergence iterations (log scale)")
+    # plt.xlabel("Convergence iteration")
+    # plt.ylabel("Count")
+    plt.tight_layout()
+    plt.show()  
 
 def loss_ratio_hist(df):
-    d_inits = df['d_init'].to_numpy()
-    d_ends = df['d_end'].to_numpy()
+    
+    fig, a =  plt.subplots(2,5,sharey=True,sharex=True)
 
-    ratios = []
-    for a,b in zip(d_inits, d_ends):
-        if (a == 0):
+    mandates = sorted(df['SEC_INVESTMENT'].unique())
+    for i,m in enumerate(mandates):
+        if (m == 1):
             continue
-        ratios.append(float(b)/float(a))
-    plt.hist(ratios, bins=100)
-    plt.yscale("log")
-    plt.title("Histogram of percent of assets lost (log scale)")
-    plt.xlabel("% of assets lost")
-    plt.ylabel("Count")
+
+        print(m)
+        mands = df.loc[df['SEC_INVESTMENT'] == m]
+
+        d_inits = mands['d_init'].to_numpy() / (1 - float(m))
+        print(d_inits)
+        d_ends = mands['d_end'].to_numpy()
+
+        convergences = mands['final_iter'].to_numpy()
+        convergences = convergences - 50
+
+
+        ratios = []
+        for start,finish in zip(d_inits, d_ends):
+            if (start != 0):
+                ratios.append((start - finish)/start)
+            else:
+                print("start is $0?")
+        
+        row = i//5
+        col = i%5
+        print(row, col)
+        # a[0, 0].hist(np.zeros(100), bins=100)
+        n, bins, patches = a[row, col].hist(ratios, bins=np.linspace(0,1,100),  cumulative=True)
+        a[row, col].set_title("Mandate = " + str(m * 100) + "%")
+        # a[row, col].set_ylim(bottom=1)
+        # a[row, col].yscale("log")
+    # plt.ylim(0, 1*10**6)
+
+    # plt.yscale("log")
+    # plt.ylim(0,10000)
+    plt.suptitle("Histogram of percent of assets lost (log scale)")
+    # plt.xlabel("% of assets lost")
+    # plt.ylabel("Count")
+    # plt.tight_layout()
     plt.show()  
 
 def make_plots(df):
-    # print(colored('  [+] Making crossover histogram', 'green'))
-    # crossover_hist(df)
-    # print(colored('  [+] Making convergence histogram', 'green'))
-    # converge_hist(df)
+    print(colored('  [+] Making crossover histogram', 'green'))
+    crossover_hist(df)
+    print(colored('  [+] Making convergence histogram', 'green'))
+    converge_hist(df)
     print(colored('  [+] Making loss ratio histogram', 'green'))
     loss_ratio_hist(df)
