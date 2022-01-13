edb27b395b8de68c225c5f81aad4c3997c8c8161
ryanpiersma <ryanpiersma131415@gmail.com>
2022-01-13 16:45:03 -0500

    basic data visualization for sim params

---
+++ scripts/dist_analyze.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 13 15:07:22 2022
+
+@author: ry_pi5
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+params = ['PERCENT_EVIL', 'PAYOFF', 'SEC_INVESTMENT', 'SEC_INVESTMENT_CONVERSION_RATE', 'WEALTH_GAP', 'ATTACK_COST_CONVERSION_RATE']
+
+def main():
+    path = '../data/df_no50.csv'
+    dframe = pd.read_csv(path, index_col=False, header=0)
+    num_bins = 10
+    num_rows = 2
+    num_cols = 3
+    i = 0
+    
+    for param in params:
+        i += 1
+        plt.subplot(num_rows, num_cols, i)
+        plt.title(param + " distribution")
+        plt.xlabel("Param val")
+        plt.ylabel("# of simulations")
+        plt.hist(dframe[param], num_bins, facecolor='blue', alpha=0.5, edgecolor='black')
+        plt.show()
+
+    
+
+    
+if __name__ == "__main__":
+    main()
