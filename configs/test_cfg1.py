4c63caca65665ee7366e6a2a2c4b808444cae414
adamhastings <itsadamhastings@gmail.com>
2022-11-01 16:49:53 -0400

    pinned values are implicit in the params, so no need to specify them. Pinned filename is also now included in the config file

--- configs/test_cfg1.py
+++ configs/test_cfg1.py
@@ -11,20 +11,17 @@ import numpy as np
 
 Format: (start, end, increment)
 """
-params_ranges = dict(
-    ATTACKERS = np.linspace(0.1, 1.0, 1), # Percentage of Attackers, relative to BLUE_PLAYERS
-    PAYOFF = np.linspace(0.1, 1.0, 1), # How much of a defender's assets are stolen if attacker is succesful
-    INEQUALITY = np.linspace(0.1, 1.0, 1), # How much poorer Attackers are than Defenders, as a pct
-    EFFICIENCY = np.linspace(0.1, 1.0, 1), # Percent of MANDATE that goes towards increasing a defender's costToAttack
-    SUCCESS = np.linspace(0.1, 1.0, 1), # Percentage of a defender's assets that must be spent by an Attacker to attempt an attack
-    CHANCE_OF_GETTING_CAUGHT = np.linspace(0.0, 0.0, 1), # Probability that an attacker is caught and has their assets confiscated
-    CLAIMS = np.linspace(0.1, 1.0, 1), # Percentage of losses paid back to defender by Insurer
-    PREMIUMS = np.linspace(0.1, 1.0, 1), # Percentage of MANDATE that is allocated towards security
-    MANDATE = np.linspace(0.0, 1.0, 11) # Percentage of assets that are spent on security measures.
+params = dict(
+    ATTACKERS = np.linspace(0.1, 1.0, 2), # Percentage of Attackers, relative to BLUE_PLAYERS
+    PAYOFF = np.linspace(0.1, 1.0, 2), # How much of a defender's assets are stolen if attacker is succesful
+    INEQUALITY = np.linspace(0.1, 1.0, 2), # How much poorer Attackers are than Defenders, as a pct
+    EFFICIENCY = np.linspace(0.1, 1.0, 2), # Percent of MANDATE that goes towards increasing a defender's costToAttack
+    SUCCESS = np.linspace(0.1, 1.0, 2), # Percentage of a defender's assets that must be spent by an Attacker to attempt an attack
+    CAUGHT = np.linspace(0.0, 0.0, 2), # Probability that an attacker is caught and has their assets confiscated
+    CLAIMS = np.linspace(0.1, 1.0, 2), # Percentage of losses paid back to defender by Insurer
+    PREMIUMS = np.linspace(0.1, 1.0, 2), # Percentage of MANDATE that is allocated towards security
+    MANDATE = np.linspace(0.0, 1.0, 1) # Percentage of assets that are spent on security measures.
 )
-
-# The above parameter that is to be split across multiple machines 
-PARALLELIZED = 'MANDATE'
 
 game_settings = dict(
     BLUE_PLAYERS = 2000,
@@ -33,4 +30,15 @@ game_settings = dict(
     EPSILON_DOLLARS = 100,
     STABLE_ITERS = 50
 )
+
+# Find the params that are pinned to a single value 
+# (and will be run on a single machine)
+_pinned_vals = {}
+for (k,v) in params.items():
+        if len(v) == 1:
+            _pinned_vals[k] = v
+
+# set global filename to be used by all worker threads
+FILENAME = "logs/stats_" + '_'.join([str(k) + "=" + str(v[0]) for k,v in _pinned_vals.items()]) + ".csv"
+
 
