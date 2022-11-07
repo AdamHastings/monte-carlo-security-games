cf18de4f868128e4f40982f6086ad8f5828163d6
adamhastings <itsadamhastings@gmail.com>
2022-11-06 20:05:57 -0500

    trying out more tests

--- configs/test_cfg1.py
+++ configs/test_cfg1.py
@@ -9,17 +9,17 @@ Format: (start, end, increment)
 params_ranges = dict(
     # Parameters that affect game initialization
     MANDATE_range = np.linspace(0.0, 1.0, 11), # Percentage of assets that are spent on security measures.
-    ATTACKERS_range = np.linspace(0.1, 0.2, 1), # Percentage of Attackers, relative to BLUE_PLAYERS
-    INEQUALITY_range = np.linspace(0.1, 0.2, 1), # How much poorer Attackers are than Defenders, as a pct
-    PREMIUM_range= np.linspace(0.1, 0.2, 1), # Percentage of MANDATE that is allocated towards security
-    EFFICIENCY_range = np.linspace(0.1, 0.2, 1), # Percent of MANDATE that goes towards increasing a defender's costToAttack
+    ATTACKERS_range = np.linspace(0.1, 1.0, 2), # Percentage of Attackers, relative to BLUE_PLAYERS
+    INEQUALITY_range = np.linspace(0.1, 1.0, 2), # How much poorer Attackers are than Defenders, as a pct
+    PREMIUM_range= np.linspace(0.1, 1.0, 2), # Percentage of MANDATE that is allocated towards security
+    EFFICIENCY_range = np.linspace(0.1, 1.0, 2), # Percent of MANDATE that goes towards increasing a defender's costToAttack
     SUCCESS_range = np.linspace(0.1, 0.2, 2), # Percentage of a defender's assets that must be spent by an Attacker to attempt an attack
 
     # Parameters that affect gameplay
     PAYOFF_range = np.linspace(0.1, 0.2, 2), # How much of a defender's assets are stolen if attacker is successful
-    CAUGHT_range = np.linspace(0.0, 0.0, 1), # Probability that an attacker is caught and has their assets confiscated
-    CLAIMS_range = np.linspace(0.1, 0.2, 1), # Percentage of losses paid back to defender by Insurer
-    TAX_range = np.linspace(0.1, 0.2, 1,), # Percentage of confiscated assets that are retained by Government
+    CAUGHT_range = np.linspace(0.0, 1.0, 2), # Probability that an attacker is caught and has their assets confiscated
+    CLAIMS_range = np.linspace(0.1, 1.0, 2), # Percentage of losses paid back to defender by Insurer
+    TAX_range = np.linspace(0.1, 0.2, 2), # Percentage of confiscated assets that are retained by Government
 )
 
 game_settings = dict(
