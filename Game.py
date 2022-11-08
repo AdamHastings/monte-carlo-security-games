a82d22160b755e50c2749a5f0990af34b9857dd7
adamhastings <itsadamhastings@gmail.com>
2022-11-08 16:23:21 -0500

    game and game driver separated

--- Game.py
+++ Game.py
@@ -18,8 +18,9 @@ A Game object is a collection of const values plus the game results
 '''
 class Game:
         
-    def __init__(self, params, Attackers, Defenders, Insurer, Government):
+    def __init__(self, game_settings, params, Attackers, Defenders, Insurer, Government):
         self.params = params
+        self.game_settings = game_settings
         self.Defenders = Defenders
         self.Attackers = Attackers
         self.Insurer = Insurer
@@ -40,11 +41,11 @@ class Game:
         self.defender_iter_sum = 0
         self.attacker_iter_sum = 0
 
-        self.last_delta_defenders_changes = [cfg.game_settings["EPSILON_DOLLARS"]] * cfg.game_settings["DELTA_ITERS"]
-        self.last_delta_attackers_changes = [cfg.game_settings["EPSILON_DOLLARS"]] * cfg.game_settings["DELTA_ITERS"]
+        self.last_delta_defenders_changes = [self.game_settings["EPSILON_DOLLARS"]] * self.game_settings["DELTA_ITERS"]
+        self.last_delta_attackers_changes = [self.game_settings["EPSILON_DOLLARS"]] * self.game_settings["DELTA_ITERS"]
 
-        self.outside_epsilon_count_attackers = cfg.game_settings["DELTA_ITERS"]
-        self.outside_epsilon_count_defenders = cfg.game_settings["DELTA_ITERS"]
+        self.outside_epsilon_count_attackers = self.game_settings["DELTA_ITERS"]
+        self.outside_epsilon_count_defenders = self.game_settings["DELTA_ITERS"]
 
     def __str__(self):
         ret = ""
@@ -127,23 +128,23 @@ class Game:
 
     def is_equilibrium_reached(self):
 
-        last_delta_defenders_pop = self.last_delta_defenders_changes[self.iter_num % cfg.game_settings["DELTA_ITERS"]]
-        last_delta_attackers_pop = self.last_delta_attackers_changes[self.iter_num % cfg.game_settings["DELTA_ITERS"]]
-        self.last_delta_defenders_changes[self.iter_num % cfg.game_settings["DELTA_ITERS"]] = self.defender_iter_sum
-        self.last_delta_attackers_changes[self.iter_num % cfg.game_settings["DELTA_ITERS"]] = self.attacker_iter_sum
+        last_delta_defenders_pop = self.last_delta_defenders_changes[self.iter_num % self.game_settings["DELTA_ITERS"]]
+        last_delta_attackers_pop = self.last_delta_attackers_changes[self.iter_num % self.game_settings["DELTA_ITERS"]]
+        self.last_delta_defenders_changes[self.iter_num % self.game_settings["DELTA_ITERS"]] = self.defender_iter_sum
+        self.last_delta_attackers_changes[self.iter_num % self.game_settings["DELTA_ITERS"]] = self.attacker_iter_sum
      
-        if self.defender_iter_sum >= cfg.game_settings["EPSILON_DOLLARS"]:
-            if last_delta_defenders_pop < cfg.game_settings["EPSILON_DOLLARS"]:
+        if self.defender_iter_sum >= self.game_settings["EPSILON_DOLLARS"]:
+            if last_delta_defenders_pop < self.game_settings["EPSILON_DOLLARS"]:
                 self.outside_epsilon_count_defenders += 1
         else:
-            if last_delta_defenders_pop >= cfg.game_settings["EPSILON_DOLLARS"]:
+            if last_delta_defenders_pop >= self.game_settings["EPSILON_DOLLARS"]:
                 self.outside_epsilon_count_defenders -= 1
 
-        if self.attacker_iter_sum >= cfg.game_settings["EPSILON_DOLLARS"]:
-            if last_delta_attackers_pop < cfg.game_settings["EPSILON_DOLLARS"]:
+        if self.attacker_iter_sum >= self.game_settings["EPSILON_DOLLARS"]:
+            if last_delta_attackers_pop < self.game_settings["EPSILON_DOLLARS"]:
                 self.outside_epsilon_count_attackers += 1
         else:
-            if last_delta_attackers_pop >= cfg.game_settings["EPSILON_DOLLARS"]:
+            if last_delta_attackers_pop >= self.game_settings["EPSILON_DOLLARS"]:
                 self.outside_epsilon_count_attackers -= 1
 
         assert self.outside_epsilon_count_attackers >= 0, f'{self.params}'
@@ -180,7 +181,7 @@ class Game:
 
         self.final_iter = self.iter_num
         if len(self.Defenders) > 0 and len(self.Attackers) > 0:
-            assert self.final_iter >= cfg.game_settings['DELTA_ITERS'], f'{self.params}'
+            assert self.final_iter >= self.game_settings['DELTA_ITERS'], f'{self.params}'
 
         if self.crossover >= 0:
             assert self.d_end < self.a_end, f'{self.params}'
@@ -189,7 +190,7 @@ class Game:
   
     def run_iterations(self):
 
-        for self.iter_num in range(1, cfg.game_settings['SIM_ITERS']+1):
+        for self.iter_num in range(1, self.game_settings['SIM_ITERS']+1):
 
             # Make the pairings between Attackers and Defenders random
             random.shuffle(self.Attackers)
@@ -231,152 +232,3 @@ class Game:
         # Condition #3: The game did not converge after SIM_ITERS iterations 
         self.conclude_game('N')
         return
-
-def run_games(ATTACKERS, PAYOFF, INEQUALITY, EFFICIENCY, SUCCESS, CAUGHT, CLAIMS, PREMIUM, TAX, MANDATE):
-
-    global cfg
-    global gDefenders 
-    global gAttackers
-    global gInsurer
-    global gGovernment
-
-    params = {}
-    params["MANDATE"] = MANDATE
-    params["ATTACKERS"] = ATTACKERS
-    params["INEQUALITY"] = INEQUALITY
-    params["PREMIUM"] = PREMIUM
-    params["EFFICIENCY"] = EFFICIENCY
-    params["SUCCESS"] = SUCCESS
-    
-    params["PAYOFF"] = PAYOFF
-    params["CAUGHT"] = CAUGHT
-    params["CLAIMS"] = CLAIMS 
-    params["TAX"] = TAX
-    
-
-
-    for i in range(cfg.game_settings['NUM_GAMES']):
-        # print(params)
-        
-        # Create Agents here
-        Defenders = deepcopy(gDefenders)
-        Insurer = deepcopy(gInsurer)
-        Government = deepcopy(gGovernment)
-        Attackers = deepcopy(gAttackers[:int(cfg.game_settings["BLUE_PLAYERS"] * params["ATTACKERS"])])
-        
-        for a in Attackers:
-            a.assets *= params['INEQUALITY']
-            assert a.assets > 0, f'{params}'
-
-        for d in Defenders:
-            investment = d.assets * params["MANDATE"]
-            
-            insurance_investment = investment * params["PREMIUM"]
-            d.lose(insurance_investment)
-            Insurer.gain(insurance_investment)
-
-            sec_investment = investment - insurance_investment
-            d.lose(sec_investment)
-            d.costToAttack = d.assets * params["SUCCESS"]
-            d.costToAttack += (sec_investment * params["EFFICIENCY"])
-
-            assert d.assets >=0, f'{params}'
-
-        # Create a Game object to hold game parameters
-        g = Game(params=params, Attackers=Attackers, Defenders=Defenders, Insurer=Insurer, Government=Government)
-        
-        g.run_iterations()
-        
-        log = open(cfg.LOGFILE, 'a')  # write mode
-        log.write(str(g))
-        log.close()
-
-
-'''
-    Make sure you don't accidentally overwrite an existing logfile
-'''
-def init_logs(cfg):
-    if os.path.exists(cfg.LOGFILE):
-        response = input("\nThis file already exists: " + cfg.LOGFILE + "\nDo you want to replace it? Y/n\n >> ")
-        if response.lower() != "y":
-            print("\nOK, this program will not overwrite " + cfg.LOGFILE + ".\nThis program will now exit.\n")
-            sys.exit(0)
-        else:
-            print("\n")
-    print("Writing to", cfg.LOGFILE)
-
-    log = open(cfg.LOGFILE, 'w')  # write mode
-    header = ""
-    for k in sorted(cfg.params_ranges.keys()):
-        header += k[:-6] + "," # trim off the "_range" of the cfg param names
-    header += "d_init,d_end,a_init,a_end,i_init,i_end,crossover,insurer_tod,paid_claims,final_iter,reason\n"
-    log.write(header)
-    log.close()
-
-
-'''
-    Initialize worker processes with some global variables.
-    This is so that we don't have to do create 
-    Called once per core.
-'''
-def init_worker(cfg_in):
-    random.seed(3) # Seed random so that Agents are the same across multiple cores
-
-    global cfg
-    cfg = cfg_in
-
-    global gDefenders 
-    global gAttackers
-    global gInsurer
-    global gGovernment
-
-    gDefenders = []    
-    for _ in range(cfg.game_settings['BLUE_PLAYERS']):
-        gDefenders.append(Defender())
-    
-    gAttackers = []
-    for _ in range(cfg.game_settings['BLUE_PLAYERS']):
-        gAttackers.append(Attacker())
-
-    gInsurer = Insurer()
-    gGovernment = Government()
-
-
-def main():
-    try:
-        cfg = importlib.import_module("configs." + sys.argv[1])
-        if len(sys.argv) != 2:
-            raise Exception("\nERROR: Incorrect number of args!")
-    except Exception as e:
-        print(e)
-        print("\nExample of how to run config test_cfg1")
-        print("\n     $ python3 game.py test_cfg1\n\n")
-        sys.exit(0)
-
-    init_logs(cfg)
-
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-
-    inputs = list(itertools.product(cfg.params_ranges["ATTACKERS_range"],
-                                        cfg.params_ranges["PAYOFF_range"],
-                                        cfg.params_ranges["INEQUALITY_range"],
-                                        cfg.params_ranges["EFFICIENCY_range"],
-                                        cfg.params_ranges["SUCCESS_range"],
-                                        cfg.params_ranges["CAUGHT_range"],
-                                        cfg.params_ranges["CLAIMS_range"],
-                                        cfg.params_ranges["PREMIUM_range"],
-                                        cfg.params_ranges["TAX_range"],
-                                        cfg.params_ranges["MANDATE_range"]
-                                    ))
-
-    print("Starting", len(inputs), "games at", current_time)
-    with Pool(initializer=init_worker, initargs=(cfg,)) as p:
-        p.starmap(run_games, inputs)
-
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    print("\nFinished! at", current_time)
-    
-if __name__== "__main__":
-  main()
---
+++ main.py
@@ -0,0 +1,164 @@
+
+from fileinput import filename
+from datetime import datetime
+from Agent import *
+from Game import *
+from multiprocessing import Pool
+import itertools
+import numpy as np
+import importlib
+import sys
+import random
+from numpy.random import choice
+from copy import deepcopy
+import math
+import os.path
+
+def run_games(ATTACKERS, PAYOFF, INEQUALITY, EFFICIENCY, SUCCESS, CAUGHT, CLAIMS, PREMIUM, TAX, MANDATE):
+
+    global cfg
+    global gDefenders 
+    global gAttackers
+    global gInsurer
+    global gGovernment
+
+    params = {}
+    params["MANDATE"] = MANDATE
+    params["ATTACKERS"] = ATTACKERS
+    params["INEQUALITY"] = INEQUALITY
+    params["PREMIUM"] = PREMIUM
+    params["EFFICIENCY"] = EFFICIENCY
+    params["SUCCESS"] = SUCCESS
+    
+    params["PAYOFF"] = PAYOFF
+    params["CAUGHT"] = CAUGHT
+    params["CLAIMS"] = CLAIMS 
+    params["TAX"] = TAX
+    
+
+
+    for i in range(cfg.game_settings['NUM_GAMES']):
+        # print(params)
+        
+        # Create Agents here
+        Defenders = deepcopy(gDefenders)
+        Insurer = deepcopy(gInsurer)
+        Government = deepcopy(gGovernment)
+        Attackers = deepcopy(gAttackers[:int(cfg.game_settings["BLUE_PLAYERS"] * params["ATTACKERS"])])
+        
+        for a in Attackers:
+            a.assets *= params['INEQUALITY']
+            assert a.assets > 0, f'{params}'
+
+        for d in Defenders:
+            investment = d.assets * params["MANDATE"]
+            
+            insurance_investment = investment * params["PREMIUM"]
+            d.lose(insurance_investment)
+            Insurer.gain(insurance_investment)
+
+            sec_investment = investment - insurance_investment
+            d.lose(sec_investment)
+            d.costToAttack = d.assets * params["SUCCESS"]
+            d.costToAttack += (sec_investment * params["EFFICIENCY"])
+
+            assert d.assets >=0, f'{params}'
+
+        # Create a Game object to hold game parameters
+        g = Game(game_settings=cfg.game_settings, params=params, Attackers=Attackers, Defenders=Defenders, Insurer=Insurer, Government=Government)
+        
+        g.run_iterations()
+        
+        log = open(cfg.LOGFILE, 'a')  # write mode
+        log.write(str(g))
+        log.close()
+
+
+'''
+    Make sure you don't accidentally overwrite an existing logfile
+'''
+def init_logs(cfg):
+    if os.path.exists(cfg.LOGFILE):
+        response = input("\nThis file already exists: " + cfg.LOGFILE + "\nDo you want to replace it? Y/n\n >> ")
+        if response.lower() != "y":
+            print("\nOK, this program will not overwrite " + cfg.LOGFILE + ".\nThis program will now exit.\n")
+            sys.exit(0)
+        else:
+            print("\n")
+    print("Writing to", cfg.LOGFILE)
+
+    log = open(cfg.LOGFILE, 'w')  # write mode
+    header = ""
+    for k in sorted(cfg.params_ranges.keys()):
+        header += k[:-6] + "," # trim off the "_range" of the cfg param names
+    header += "d_init,d_end,a_init,a_end,i_init,i_end,crossover,insurer_tod,paid_claims,final_iter,reason\n"
+    log.write(header)
+    log.close()
+
+
+'''
+    Initialize worker processes with some global variables.
+    This is so that we don't have to do create 
+    Called once per core.
+'''
+def init_worker(cfg_in):
+    random.seed(3) # Seed random so that Agents are the same across multiple cores
+
+    global cfg
+    cfg = cfg_in
+
+    global gDefenders 
+    global gAttackers
+    global gInsurer
+    global gGovernment
+
+    gDefenders = []    
+    for _ in range(cfg.game_settings['BLUE_PLAYERS']):
+        gDefenders.append(Defender())
+    
+    gAttackers = []
+    for _ in range(cfg.game_settings['BLUE_PLAYERS']):
+        gAttackers.append(Attacker())
+
+    gInsurer = Insurer()
+    gGovernment = Government()
+
+
+def main():
+    try:
+        cfg = importlib.import_module("configs." + sys.argv[1])
+        if len(sys.argv) != 2:
+            raise Exception("\nERROR: Incorrect number of args!")
+    except Exception as e:
+        print(e)
+        print("\nExample of how to run config test_cfg1")
+        print("\n     $ python3 game.py test_cfg1\n\n")
+        sys.exit(0)
+
+    init_logs(cfg)
+
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
+    inputs = list(itertools.product(cfg.params_ranges["ATTACKERS_range"],
+                                        cfg.params_ranges["PAYOFF_range"],
+                                        cfg.params_ranges["INEQUALITY_range"],
+                                        cfg.params_ranges["EFFICIENCY_range"],
+                                        cfg.params_ranges["SUCCESS_range"],
+                                        cfg.params_ranges["CAUGHT_range"],
+                                        cfg.params_ranges["CLAIMS_range"],
+                                        cfg.params_ranges["PREMIUM_range"],
+                                        cfg.params_ranges["TAX_range"],
+                                        cfg.params_ranges["MANDATE_range"]
+                                    ))
+
+    print("Starting", len(inputs), "games at", current_time)
+    with Pool(initializer=init_worker, initargs=(cfg,)) as p:
+        p.starmap(run_games, inputs)
+
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("\nFinished! at", current_time)
+    
+if __name__== "__main__":
+  main()
