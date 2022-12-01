fda0310679ef4bda4101f18a082035b771340493
adamhastings <itsadamhastings@gmail.com>
2022-12-01 16:21:26 -0500

    cleanup

--- cpp-games/main.cpp
+++ cpp-games/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+#include "oneapi/tbb.h"
+
+
+using namespace oneapi::tbb;
+
+
+void Foo(float x) {
+    std::cout << x << std::endl;
+}
+
+
+void ParallelApplyFoo(std::vector<float> v, size_t n) {
+    parallel_for(size_t(0), n, [=](size_t i) {Foo(v[i]);});
+}
+
 
 int main() {
-    cout << "Hello world" << endl;
+    std::cout << "Hello world" << std::endl;
+
+    std::vector<float> v;
+
+    for (auto i = 0; i < 200; i++) {
+        v.push_back(i);
+    }
+    ParallelApplyFoo(v, v.size());
     return 0;
 }
