89f983275fd33dea4cd5a929b3884c25ee97b983
adamhastings <itsadamhastings@gmail.com>
2022-12-05 00:40:51 -0500

    adds runtime information

--- cpp-games/main.cpp
+++ cpp-games/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <chrono>
+#include <ctime>  
 #include "oneapi/tbb.h"
 #include "Player.h"
 #include "Game.h"
@@ -139,12 +141,21 @@ int main() {
     cfg c = cfg();
     vector<Params> cart = c.make_all_params();
 
+
+    auto start = std::chrono::system_clock::now();
+    std::time_t start_time = std::chrono::system_clock::to_time_t(start);
+    std::cout << "started computation at " << std::ctime(&start_time);
+
     ParallelRunGames(cart, cart.size());
 
-    Player p = Player();
-    cout << p.get_assets() << endl;
-    p.gain(100);
-    cout << p.get_assets() << endl;
+    auto end = std::chrono::system_clock::now();
+ 
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+ 
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds.count() << "s"
+              << std::endl;
 
     return 0;
 }
