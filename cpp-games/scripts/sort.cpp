e93c7699f831e62a762adf5891e3ff3ba883d252
adamhastings <itsadamhastings@gmail.com>
2023-03-10 13:08:00 -0500

    sort across all of a single mandate at once

--- cpp-games/scripts/sort.cpp
+++ cpp-games/scripts/sort.cpp
@@ -43,21 +43,6 @@ struct LineStruct {
 
     bool operator < (const LineStruct& lineB) const
     {
-        // if (CLAIMS != lineB.CLAIMS) {
-            // return CLAIMS < lineB.CLAIMS;
-        // } else if (CAUGHT != lineB.CAUGHT){
-            // return CAUGHT < lineB.CAUGHT;
-        // } else if (PAYOFF != lineB.PAYOFF) {
-            // return PAYOFF < lineB.PAYOFF;
-        // } else if (EFFORT != lineB.EFFORT) {
-            // return EFFORT < lineB.EFFORT;
-        // } else if (EFFICIENCY != lineB.EFFICIENCY) {
-            // return EFFICIENCY < lineB.EFFICIENCY;
-        // } else if (INEQUALITY != lineB.INEQUALITY) {
-            // return INEQUALITY < lineB.INEQUALITY;
-        // } else {
-            // return ATTACKERS < lineB.ATTACKERS;
-        // }
 
         if      (ATTACKERS  != lineB.ATTACKERS ) return ATTACKERS  < lineB.ATTACKERS;
         else if (INEQUALITY != lineB.INEQUALITY) return INEQUALITY < lineB.INEQUALITY;
@@ -72,81 +57,94 @@ struct LineStruct {
 
 int main(int argc, char* argv[]) {
 
+
+    if (argc != 2) {
+        cerr << "Must provide input MANDATE! E.g. ./sort 0.2" << endl;
+        return -1;
+    }
+
+    string mandate = argv[1];
+
+    // string filename = argv[1];
+    // ifstream infile(filename);
+    // if (!infile.good()) {
+    //     cerr << "bad input filename" << endl;
+    //     return -1;
+    // }
+
     // string filename = "../split/MANDATE=0.0_TAX=0.0_PREMIUM=0.0.csv";
-    string filename = "../split/test.csv";
-    ifstream infile(filename);
+    // string filename = "../split/MANDATE=0.2_TAX=0.0_PREMIUM=0.0.csv";
 
-    string line;
+    const int TAX_size = 11;
+    const int PREMIUM_size=11;
 
-    string ofilename = "../sorted/sorted_test.csv";
-    ofstream out;
-    out.open(ofilename);
-
-
-    getline(infile, line); // throwaway first line
-    string firstline = line;
-    // cout << firstline << endl;
-
-    out << firstline << endl;
-
-    vector<LineStruct> lines;
-
-    int count = 0;
-    while(getline(infile, line)) {
-        vector<string> split = splitline(line);
-
-        LineStruct ls;
-        ls.idx = count;
-        ls.full_line  = line;
-        ls.ATTACKERS  = (int) round(stod(split[1]) * 10);
-        ls.INEQUALITY = (int) round(stod(split[2]) * 10);
-        ls.EFFICIENCY = (int) round(stod(split[4]) * 10);
-        ls.EFFORT     = (int) round(stod(split[5]) * 10);
-        ls.PAYOFF     = (int) round(stod(split[6]) * 10);
-        ls.CAUGHT     = (int) round(stod(split[7]) * 10);
-        ls.CLAIMS     = (int) round(stod(split[8]) * 10);
-
-        count += 1;
-        // cout << ls.full_line << endl;
-
-        if (ls.full_line == "") {
-            cout << "it's empty!" << endl;
-            while(1);
-        }
+    for (int i=0; i<TAX_size; i++) {
+        for (int j=0; j<PREMIUM_size; j++) {
+            
+            string taxstr = i<10 ? "0." + to_string(i): "1.0";
+            string premstr = j<10 ? "0." + to_string(j): "1.0";
+            string ifilename = "../split/MANDATE=" + mandate + "_TAX=" + taxstr + "_PREMIUM=" + premstr + ".csv";
+            string ofilename = "../sorted/MANDATE=" + mandate + "_TAX=" + taxstr + "_PREMIUM=" + premstr + ".csv";
+            cout << ifilename << ": " << std::flush;
         
-        // if (ls.CAUGHT == 0) {
-        //     cout << ls.ATTACKERS << ", " << ls.ATTACKERS << ", " << ls.INEQUALITY << ", " << ls.EFFICIENCY << ", " << ls.EFFORT << ", " << ls.PAYOFF << ", " << ls.CAUGHT << ", " << ls.CLAIMS << endl;
-        // }
-        // cout << split[7] << " - " << stod(split[7]) << " - " << round(stod(split[7]) * 10) << " -> " << ls.CAUGHT << endl;
+            ifstream infile(ifilename);
 
-        lines.push_back(ls);
-    }
+            string line;
 
-    cout << "All lines loaded. Attempting to sort..." << endl;
+            ofstream out;
+            out.open(ofilename);
 
-    for (auto ls : lines) {
-        if (ls.full_line == "") {
-            cout << ls.ATTACKERS << ", " << ls.INEQUALITY << ", " << ls.EFFICIENCY << ", " << ls.EFFORT << ", " << ls.PAYOFF << ", " << ls.CAUGHT << ", " << ls.CLAIMS << endl;
-            cout << ls.full_line << endl;
+
+            getline(infile, line); // throwaway first line
+            string firstline = line;
+            out << firstline << endl;
+
+            vector<LineStruct> lines;
+
+            int count = 0;
+            while(getline(infile, line)) {
+                vector<string> split = splitline(line);
+
+                LineStruct ls;
+                ls.idx = count;
+                ls.full_line  = line;
+                ls.ATTACKERS  = (int) round(stod(split[1]) * 10);
+                ls.INEQUALITY = (int) round(stod(split[2]) * 10);
+                ls.EFFICIENCY = (int) round(stod(split[4]) * 10);
+                ls.EFFORT     = (int) round(stod(split[5]) * 10);
+                ls.PAYOFF     = (int) round(stod(split[6]) * 10);
+                ls.CAUGHT     = (int) round(stod(split[7]) * 10);
+                ls.CLAIMS     = (int) round(stod(split[8]) * 10);
+
+                count += 1;
+                lines.push_back(ls);
+            }
+
+            cout << "Loaded. Sorting..." << std::flush;
+
+            for (auto ls : lines) {
+                if (ls.full_line == "") {
+                    cout << ls.ATTACKERS << ", " << ls.INEQUALITY << ", " << ls.EFFICIENCY << ", " << ls.EFFORT << ", " << ls.PAYOFF << ", " << ls.CAUGHT << ", " << ls.CLAIMS << endl;
+                    cout << ls.full_line << endl;
+                }
+            }
+
+
+            sort(lines.begin(), lines.end());
+
+            cout << "done! Writing..." << std::flush;
+
+            
+            for (auto ls : lines) {
+                out << ls.full_line << endl;
+            }
+
+            out.close();
+
+            cout << "done!" << endl;
+
         }
     }
-
-
-    sort(lines.begin(), lines.end());
-
-    cout << "done sorting" << endl;
-
-    
-    for (auto ls : lines) {
-        // cout << line.full_line << endl;
-        out << ls.full_line << endl;
-    }
-
-    out.close();
-
-    
-
-
 
     return 0;
 }
