e02b2a3f84df1290c3bfeb693842b006cb478e1a
adamhastings <itsadamhastings@gmail.com>
2023-03-14 16:44:59 -0400

    i'll be honest I don't remember what this is..I think it's superceded by parse_sorted.cp

--- cpp-games/scripts/find_best.cpp
+++ cpp-games/scripts/find_best.cpp
@@ -13,17 +13,42 @@ int main(int argc, char* argv[]) {
 
     for (int i=0; i<TAX_size; i++) {
         for (int j=0; j<PREMIUM_size; j++) {
+
+            
             string taxstr = i<10 ? "0." + to_string(i): "1.0";
             string premstr = j<10 ? "0." + to_string(j): "1.0";
             // string ofilename = "../split/MANDATE=" + mandate + "_TAX=" + taxstr + "_PREMIUM=" + premstr + ".csv";
             // out[i][j].open(ofilename);
             // out[i][j] << firstline << endl;
-            ifstream ins[i];
+            cout << "TAX=" << taxstr << "_PREMIUM=" << premstr << ": " << flush;
+
+            vector<ifstream> ins;
 
             for (string m : mrange) {
                 string filename = "../sorted/MANDATE=" + m + "_TAX=" + taxstr + "_PREMIUM=" + premstr + ".csv";
-                cout << filename << endl;
+                // cout << filename << endl;
+                ifstream in;
+                in.open(filename);
+                ins.push_back(in);
             }
+
+            cout << "Loaded! Parsing..." << flush;
+
+            vector<string> lines;
+            int linecount = 0;
+
+            // for (ifstream in : ins) {
+            //     getline(in, )
+            // }
+
+            for (int m=0; m<ins.size(); m++) {
+                string line
+                getline()
+            }
+
+            // TODO parse
+
+            cout << "done!" << endl;
         }
     }
 }
