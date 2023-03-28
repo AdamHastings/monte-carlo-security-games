71d8522e49337abccda6f04b07c64831337e2c13
adamhastings <itsadamhastings@gmail.com>
2023-03-28 14:43:22 -0400

    helper script

---
+++ cpp-games/run_all_ows.sh
@@ -0,0 +1,12 @@
+source tbb/latest/env/vars.sh
+
+./run_games ows_ATTACKERS.json
+./run_games ows_CAUGHT.json
+./run_games ows_CLAIMS.json
+./run_games ows_EFFICIENCY.json
+./run_games ows_EFFORT.json
+./run_games ows_INEQUALITY.json
+./run_games ows_MANDATE.json
+./run_games ows_PAYOFF.json
+./run_games ows_PREMIUM.json
+./run_games ows_TAX.json
