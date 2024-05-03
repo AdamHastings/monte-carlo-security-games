9c6dd4dfa4371bb84318826d312aefdae02c44f9
adamhastings <itsadamhastings@gmail.com>
2024-05-03 16:29:19 -0400

    attack rates by industry to find mean and std dev

---
+++ parameters/scripts/curve_fit_attack_rate.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy as sp 
+from scipy import stats 
+from scipy.stats import norm
+from scipy.optimize import curve_fit 
+
+
+df = pd.read_csv("../data/attack_rates_by_industry.csv")
+
+# for row in df.rows():
+#     print(row)
+
+
+attack_rates = []
+
+for index, row in df.iterrows():
+    count = row['N']
+    attack_failure = row['Hit by ransomware']
+    vals = [attack_failure] * count
+    attack_rates.extend(vals)
+
+
+plt.hist(attack_rates, bins=len(df), density=True, alpha=0.6, color='g',  ec='black')
+
+
+
+mu_, std_ = norm.fit(attack_rates)
+
+
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 100)
+p = norm.pdf(x, mu_, std_)
+plt.plot(x, p, 'k', linewidth=2)
+
+title = "Fit results: mu = %.2f,  std = %.2f" % (mu_, std_)
+plt.title(title)
+plt.xlabel("Attack rates")
+plt.ylabel("PDF")
+
+plt.savefig("../figures/attackrate_fitting/hist.png")
