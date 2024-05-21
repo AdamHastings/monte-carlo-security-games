289044fed0addfb19b189983aa5b3815d110eed8
adamhastings <itsadamhastings@gmail.com>
2024-05-21 14:06:42 -0400

    some earlier uncommitted changes. Prints to a file instead of to terminal. Same result. But this doesn't square with (currently uncommited) is_attacking_worth_it_exploration.py?

--- parameters/scripts/simulate_attackers_earnings.py
+++ parameters/scripts/simulate_attackers_earnings.py
@@ -17,9 +17,10 @@ num_attackers = 50
 
 expected_posture_mu = 0.28
 
+f = open("simulate_attackers_earnings.txt", "w")
+
 attackers_wealths = np.random.lognormal(mean=mu, sigma=sigma, size=num_attackers) * 10 ** 9 * inequality
-# print("attacker_wealth: ", "{:e}".format(attackers_wealth))
-print("attackers_wealths: ", attackers_wealths)
+# f.write("attacker_wealth: ", "{:e}".format(attackers_wealth))
 
 num_attacks = 100 # TODO TODO TODO this is assuming that the attacker had enough to gamble for each of these attacks?
 prob_attack_success = 1 - expected_posture_mu
@@ -35,7 +36,7 @@ MAGIC_SCALAR = 0.01026
 
 
 costs_to_attack = [MAGIC_SCALAR * x[0] * x[1] for x in zip(wealths, postures)]
-# print(costs_to_attack)
+# f.write(costs_to_attack)
 total_costs_to_attack = np.sum(costs_to_attack)
 
 
@@ -46,12 +47,12 @@ successful_attacks = wealths[0: num_success_attacks]
 ransoms = ransom_base * wealths ** ransom_exp
 
 total_revenue = np.sum(ransoms)
-print("sample revenue: ", "{:e}".format(total_revenue))
-print("sample costs: ", "{:e}".format(total_costs_to_attack))
+f.write("sample revenue: {:e}\n".format(total_revenue))
+f.write("sample costs: {:e}\n".format(total_costs_to_attack))
 
 
-print("sample R/C = ", total_revenue/total_costs_to_attack)
-print("")
+f.write("sample R/C = {}\n".format(total_revenue/total_costs_to_attack))
+f.write("")
 
 
 
@@ -61,7 +62,7 @@ target_rc_ratio = 0.33
 
 expected_assets = np.exp(mu + (sigma **2 / 2) ) * 10**9
 
-print("expected_assets: ", "{:e}".format(expected_assets))
+f.write("expected_assets: {:e}\n".format(expected_assets))
 
 analytic_revenue = num_success_attacks * ransom_base * expected_assets ** ransom_exp
 
@@ -75,24 +76,26 @@ analytic_rc_ratio = analytic_revenue / analytic_costs
 
 
 
-print("analytic_revenue: ", "{:e}".format(analytic_revenue))
-print("analytic_costs: ", "{:e}".format(analytic_costs))
-print("analytic_rc_ratio: ", analytic_rc_ratio)
-print("analytic_magic_scalar: ", analytic_magic_scalar)
+f.write("analytic_revenue: {:e}\n".format(analytic_revenue))
+f.write("analytic_costs:{:e}\n".format(analytic_costs))
+f.write("analytic_rc_ratio: {}\n".format(analytic_rc_ratio))
+f.write("analytic_magic_scalar: {}\n".format(analytic_magic_scalar))
 
 
-print("")
-print("Example cost_to_attacks:")
+f.write("\n")
+f.write("Example cost_to_attacks:\n")
 for i in range(50):
     cost_to_attack = wealths[i] *  postures[i] * analytic_magic_scalar
     expected_loot = ransoms[i] * (1 - postures[i]) 
     worth_attacking = "YES" if expected_loot > cost_to_attack else "NO"
     have_enough = "YES" if cost_to_attack < attackers_wealths[i] else "NO"
-    fstring = "  -- wealth = {:.2e}\t posture = {}\t\t cost to attack = {:.2e}\t ransom = {:.2e}\t expected_loot = {:2e}\t worth attacking? {}\t have_enough? {}".format(wealths[i], round(postures[i],2), cost_to_attack, ransoms[i], expected_loot, worth_attacking, have_enough)
-    print(fstring)
+    fstring = "  -- wealth = {:.2e}\t posture = {}\t\t cost to attack = {:.2e}\t ransom = {:.2e}\t expected_loot = {:2e}\t worth attacking? {}\t have_enough? {}\n".format(wealths[i], round(postures[i],2), cost_to_attack, ransoms[i], expected_loot, worth_attacking, have_enough)
+    f.write(fstring)
 
 
 plt.hist(ransoms, bins=100)
 plt.xlabel("earnings")
 plt.ylabel("count")
 plt.savefig("../figures/simulated_attacker_earnings/simulated_attacker_earnings.png")
+
+f.close()
