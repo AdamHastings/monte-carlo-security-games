c3dd14a624e33fb931ff8e3c79a0faea8d35390d
adamhastings <itsadamhastings@gmail.com>
2024-08-15 14:23:09 -0400

    erf plot

---
+++ parameters/scripts/plot_erf.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from matplotlib import rc
+
+
+
+x = np.arange(0, .1, 0.001)
+# x = np.append(x, 1)
+
+SCALING_FACTOR = 25
+
+plt.figure(figsize=(4,3))
+ax = plt.subplot(111)
+
+y = [math.erf(xi * SCALING_FACTOR) for xi in x]
+ax.plot(x, y)
+
+# y2 = [(xi / (1 + abs(xi))) for xi in x]
+# plt.plot(x, y2)
+
+# plt.plot(x, [math.erf(xi) for xi in x])
+
+
+plt.xlabel("Total investment (capex + opex)")
+plt.ylabel("security posture")
+# plt.ylabel("security posture \n " + r'\textcolor{red}{in-model average posture} ' )                 ")
+# plt.ylabel(r'\textcolor{red}{Today} '+
+#            r'\textcolor{green}{is} '+
+#            r'\textcolor{blue}{cloudy.}')
+
+ax.axvline(x = 0.01, ymin = 0.0, ymax = 0.29, color = 'r', label = 'axvline - % of full height')
+ax.axhline(y = 0.28, xmin =0, xmax = 0.13, color = 'r', label = 'axvline - % of full height')
+
+
+ticks = np.arange(0, .11, .02)
+ticks = np.insert(ticks,1, 0.01)
+labels = ["{:.0%}".format(x) for x in ticks]
+labels[1] = "1% \nreal-world average\n security investment"
+
+ax.set_xticks(ticks, labels)
+id_tick_change_colour = 1 
+plt.setp(ax.get_xticklabels()[id_tick_change_colour], color='red')
+
+
+# yticks = np.arange(0, 1.1, .5)
+# yticks = np.insert(yticks, 1, 0.28)
+yticks = np.array([0.28, .5, 1])
+ylabels = [str(x) for x in yticks]
+ylabels[0] = "\n\n\n0.28 \nin-model\naverage\nposture"
+
+ax.set_yticks(yticks, ylabels)
+id_tick_change_colour = 0
+plt.setp(ax.get_yticklabels()[id_tick_change_colour], color='red')
+
+
+plt.tight_layout()
+plt.savefig("../figures/erf/erf.png")
+plt.savefig("../figures/erf/erf.pdf")
