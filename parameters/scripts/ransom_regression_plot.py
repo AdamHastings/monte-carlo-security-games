6e29a69bab4fd5384cc867c950a7c4ce3b56e737
adamhastings <itsadamhastings@gmail.com>
2024-08-15 15:17:40 -0400

    ransom regression figure

---
+++ parameters/scripts/ransom_regression_plot.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt 
+import matplotx
+import numpy as np
+
+
+revenues = [
+    0.00,
+    10000000.00,
+    50000000.00,
+    250000000.00,
+    500000000.00,
+    1000000000.00,
+    5000000000.00,
+]
+
+recovery_costs = [
+    1222487.00,
+    1530886.00,
+    2435089.00,
+    2844296.00,
+    2581460.00,
+    2815793.00,
+    3767731.00,
+]
+
+ransom_payments = [
+    330000,
+    220000,
+    840000,
+    2000000,
+    3000000,
+    6600000
+]
+
+with plt.style.context(matplotx.styles.dufte):
+    fig, ax = plt.subplots()
+
+    ax.scatter(revenues[1:], ransom_payments, label="median ransom payment", color='red')
+
+    x = np.arange(0,5e9, 10000)
+    rans_reg = [1.21e-3 * a + 792145 for a in x]
+    ax.plot(x, rans_reg, color='red', alpha = 0.5, label=("${{{}}}x + {{{}}}$").format(1.21e-3, 792145) )
+
+    ax.scatter(revenues, recovery_costs, label="median recovery cost", color='blue')
+
+    rec_reg = [230123 * pow(a, .125) for a in x]
+    ax.plot(x, rec_reg, color='blue', alpha = 0.5, label=("${{{}}}x^{{{}}}$").format(230123, 0.125) )
+
+    ax.legend()
+    plt.xlabel("annual revenue (billions)")
+    plt.ylabel("cost (millions)")
+    plt.xlim(-5e8,5.5e9)
+    plt.ylim(-5e5, 7e6)
+    
+    plt.tight_layout()
+    plt.savefig("../figures/ransom_regression/ransom_regression.png")
+    plt.savefig("../figures/ransom_regression/ransom_regression.pdf")
