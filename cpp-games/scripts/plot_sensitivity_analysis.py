9a0b32d15b4197f3e3052de1bd7b028a8847bfb1
adamhastings <itsadamhastings@gmail.com>
2024-08-27 17:09:07 -0400

    MAX_ITERATIONS=500 gets sensitivity analysis starting point to be at 0.5. plotting split into percentage and non-percentage values

--- cpp-games/scripts/plot_sensitivity_analysis.py
+++ cpp-games/scripts/plot_sensitivity_analysis.py
@@ -12,16 +12,35 @@ baseline_vals = {
     "RANSOM_B1" : 0.00121,
     "RECOVERY_COST_BASE" : 230123,
     "RECOVERY_COST_EXP" : 0.125,
-    # "WEALTH" : ,
     "POSTURE" : 0.28,
     "POSTURE_NOISE" : 0.1,
     "NUM_QUOTES" : 10,
-    # "LOSS_RATIO" : # Separate experiment,
+    "LOSS_RATIO" : 0.8, # Separate experiment,
     "RETENTION_REGRESSION_FACTOR" : 25.0,
     "ATTACKS_PER_EPOCH" : 100,
     "CTA_SCALING_FACTOR" : 0.85714285714,
     "DEPRECIATION" : 0.4,
     "INVESTMENT_SCALING_FACTOR" : 25,
+}
+
+percentage_vars = {
+    "INEQUALITY",
+    "POSTURE",
+    "DEPRECIATION",
+    "LOSS_RATIO"
+}
+
+non_percentage_vars = {
+    "RANSOM_B0",
+    "RANSOM_B1",
+    "RECOVERY_COST_BASE",
+    "RECOVERY_COST_EXP",
+    "POSTURE_NOISE",
+    "NUM_QUOTES",
+    "RETENTION_REGRESSION_FACTOR",
+    "ATTACKS_PER_EPOCH",
+    "CTA_SCALING_FACTOR",
+    "INVESTMENT_SCALING_FACTOR",
 }
 
 
@@ -34,6 +53,7 @@ labels = {
     "POSTURE" :r'$\mu_p$',
     "POSTURE_NOISE" :r'$\sigma_p$',
     "NUM_QUOTES" :r'$Q$',
+    "LOSS_RATIO" : r'$\mathcal{L}$',
     "RETENTION_REGRESSION_FACTOR" :r'$\beta_{1,retention}$',
     "ATTACKS_PER_EPOCH" :r'$K$',
     "CTA_SCALING_FACTOR" :r'$s_c$',
@@ -42,56 +62,125 @@ labels = {
 }
 
 def plot_sensitivity_analysis():
+
+    assert(len(labels) == len(baseline_vals))
+    assert(len(baseline_vals) == len(percentage_vars) + len(non_percentage_vars))
+
     with plt.style.context(matplotx.styles.dufte):
 
-        plt.clf()
         plt.rcParams['text.usetex'] = True
 
     
-        reldir = "../logs/sweeps/MAX_ITERATIONS=100/"
-        for filename in os.listdir(reldir): # consider doing MAX_ITERATIONS=5000 as well
-            
-            # path = "../logs/sweeps/"
-            df = pd.read_csv(reldir + filename)
+        reldir = "../logs/sweeps"
+        for foldername in os.listdir("../logs/sweeps/"): # consider doing MAX_ITERATIONS=5000 as well
+            print(foldername)
+            folderpath = reldir + "/" + foldername
 
-            var = filename[6:-4] # trim off sweep_ and .csv
-            print(var)
+            plt.clf()
+            for var in percentage_vars:
+                filename = "sweep_" + var + ".csv"
+                # path = "../logs/sweeps/"
+                filepath = folderpath + "/" + filename
+                # print("  " + filepath)
 
+                try:
+                    df = pd.read_csv(filepath)
+                except:
+                    print("     " + filepath + " does not exist!")
+                    continue
 
-            if len(df) < 100:
-                print("  -- " + var + " log empty!")
-                continue
-
-            if not var in df:
-                print("  ** " + var + " key not found!")
-                continue
-
-
-            # sort by value
-            df = df.sort_values(by=[var])
-            # print(df)
-            
-            df['loss'] = 1 - df['d_end'] / df['d_init'] # == (d_init - d_end) / d_init
-            df[var] = df[var] / baseline_vals[var] # Should normalize df[val] to be within range [0,2]
-            assert(df[var].max() <= 2)
-            assert(df[var].min() >= 0)
+                var = filename[6:-4] # trim off sweep_ and .csv
+                # print(var)
 
 
-            plt.plot(df[var], df['loss'], label=labels[var])
+                if len(df) < 100:
+                    print("     " + var + " log empty!")
+                    continue
+
+                if not var in df:
+                    print("     " + var + " key not found!")
+                    continue
 
 
-        ax = plt.gca()
-        ax.grid(which='major', axis='x')
-        plt.xticks((0,1,2), ("0x", "1x\n(baseline value)", "2x"))
-        plt.xlabel("parameter value")
-        plt.ylabel("loss (lower is better)")
-        plt.ylim(0,1)
-        # plt.legend()
-        matplotx.line_labels()  # line labels to the right
+                # sort by value
+                df = df.sort_values(by=[var])
+                # print(df)
+                
+                df['loss'] = 1 - df['d_end'] / df['d_init'] # == (d_init - d_end) / d_init
+                assert(df[var].max() <= 1)
+                assert(df[var].min() >= 0)
 
-        plt.tight_layout()
-        plt.savefig("figures/sensitivity_analysis/sensitivity_analysis.pdf")
-        plt.savefig("figures/sensitivity_analysis/sensitivity_analysis.png")
+
+                plt.plot(df[var], df['loss'], label=labels[var])
+
+
+            ax = plt.gca()
+            ax.grid(which='major', axis='x')
+            # plt.xticks((0,1,2), ("0x", "1x\n(baseline value)", "2x"))
+            plt.xlabel("parameter value")
+            plt.ylabel("loss (lower is better)")
+            plt.ylim(0,1)
+            plt.xlim(0,1)
+            # plt.legend()
+            matplotx.line_labels()  # line labels to the right
+
+            plt.tight_layout()
+            plt.savefig("figures/sensitivity_analysis/sensitivity_analysis_" + foldername + "_percentage_vars.pdf")
+            plt.savefig("figures/sensitivity_analysis/sensitivity_analysis_" + foldername + "_percentage_vars.png")
+
+
+            ## Now do non-percentage vars
+            plt.clf()
+            for var in non_percentage_vars:
+                filename = "sweep_" + var + ".csv"
+                # path = "../logs/sweeps/"
+                filepath = folderpath + "/" + filename
+                # print("  " + filepath)
+
+                try:
+                    df = pd.read_csv(filepath)
+                except:
+                    print("     " + filepath + " does not exist!")
+                    continue
+
+                var = filename[6:-4] # trim off sweep_ and .csv
+                # print(var)
+
+
+                if len(df) < 100:
+                    print("     " + var + " log empty!")
+                    continue
+
+                if not var in df:
+                    print("     " + var + " key not found!")
+                    continue
+
+
+                # sort by value
+                df = df.sort_values(by=[var])
+                # print(df)
+                
+                df['loss'] = 1 - df['d_end'] / df['d_init'] # == (d_init - d_end) / d_init
+                df[var] = df[var] / baseline_vals[var] # Should normalize df[val] to be within range [0,2]
+                assert(df[var].max() <= 2)
+                assert(df[var].min() >= 0)
+
+
+                plt.plot(df[var], df['loss'], label=labels[var])
+
+
+            ax = plt.gca()
+            ax.grid(which='major', axis='x')
+            plt.xticks((0,1,2), ("0x", "1x\n(baseline value)", "2x"))
+            plt.xlabel("parameter value")
+            plt.ylabel("loss (lower is better)")
+            plt.ylim(0,1)
+            # plt.legend()
+            matplotx.line_labels()  # line labels to the right
+
+            plt.tight_layout()
+            plt.savefig("figures/sensitivity_analysis/sensitivity_analysis_" + foldername + "_non_percentage_vars.pdf")
+            plt.savefig("figures/sensitivity_analysis/sensitivity_analysis_" + foldername + "_non_percentage_vars.png")
 
 
 
