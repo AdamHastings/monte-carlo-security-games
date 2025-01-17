ddf08428424bd842aa2868b3e7e6b239b4cc4678
adamhastings <itsadamhastings@gmail.com>
2025-01-16 21:43:50 -0500

    important figs list changed

--- simulator/scripts/copy_important_figs.sh
+++ simulator/scripts/copy_important_figs.sh
@@ -13,6 +13,11 @@ important_figs=(
     "figures/fullsize_short_selfless_insurers/"
     "figures/fullsize_short_mandatory_insurance/"
     "figures/sensitivity_analysis/"
+    "figures/fullsize_short_with_asset_growth_GROWTH_RATE=0.001/"
+    "figures/fullsize_short_with_asset_growth_GROWTH_RATE=0.002/"
+    "figures/fullsize_short_with_asset_growth_GROWTH_RATE=0.01/"
+    "../../parameter-calcs/figures/erf/erf.pdf"
+    "../../parameter-calcs/figures/ransom_regression/ransom_regression.pdf"
 )
 
 for i in ${important_figs[@]}; do
