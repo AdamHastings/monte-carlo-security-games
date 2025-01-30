188d6944488d8f7adfcf39b00acf504d7e31a195
adamhastings <itsadamhastings@gmail.com>
2025-01-30 17:09:14 -0500

    figure reformatting again

--- parameter-calcs/scripts/ransom_regression_plot.py
+++ parameter-calcs/scripts/ransom_regression_plot.py
@@ -53,12 +53,12 @@ with plt.style.context(matplotx.styles.dufte):
 
     x = np.arange(0,5e9, 10000)
     rans_reg = [1.21e-3 * a + 792145 for a in x]
-    ax.plot(x, rans_reg, color='red', alpha = 0.5, label=("${{{}}}x + {{{}}}$ ($R^2 = 0.928$)").format(1.21e-3, 792145) )
+    ax.plot(x, rans_reg, color='red', alpha = 0.5, label=(" ${{{}}}x + {{{}}}$\n ($R^2 = 0.928$)").format(1.21e-3, 792145) )
 
     ax.scatter(revenues, recovery_costs, label="median recovery cost", color='blue', marker="s")
 
     rec_reg = [230123 * pow(a, .125) for a in x]
-    ax.plot(x, rec_reg, color='blue', alpha = 0.5, label=("${{{}}}x^{{{}}}$ ($R^2 = 0.865$)").format(230123, 0.125), linestyle='--' )
+    ax.plot(x, rec_reg, color='blue', alpha = 0.5, label=(" ${{{}}}x^{{{}}}$\n ($R^2 = 0.865$)").format(230123, 0.125), linestyle='--' )
 
     xticks = np.arange(0,6,1) * 1e9
     xlabels = ["$" + str(round(a / 1e9)) + "B" for a in xticks]
@@ -68,11 +68,16 @@ with plt.style.context(matplotx.styles.dufte):
     ylabels = ["$" + str(round(a / 1e6)) + "M" for a in yticks]
     ax.set_yticks(yticks, ylabels)
 
-    ax.legend(framealpha=1.0)
+    # ax.legend(framealpha=1.0)
+    matplotx.line_labels(fontsize=12)  # line labels to the right
+
+    scatter_legend_handles = [plt.Line2D([0], [0], marker='o', color='red', label='median ransom payment', linestyle='None'), plt.Line2D([0], [0], marker='s',  color='blue', label='median recovery cost', linestyle='None')]
+    ax.legend(handles=scatter_legend_handles,  loc='center left', framealpha=1.0, bbox_to_anchor=(.4, 0.15))
+
     plt.xlabel("annual revenue")
     plt.ylabel("cost per incident")
-    plt.xlim(-1e8,5.5e9)
-    plt.ylim(-5e5, 7e6)
+    plt.xlim(-1e8,5.2e9)
+    plt.ylim(-5e2, 7e6)
     plt.gca().xaxis.grid(True)
     plt.tight_layout()
     plt.savefig("../figures/ransom_regression/ransom_regression.png")
